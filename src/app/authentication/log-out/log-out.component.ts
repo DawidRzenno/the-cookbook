287016b93439fe952baec7f137abb7aec6be3e12
Dawid Rzenno <dawid.rzenno@protonmail.com>
2020-03-26 13:38:45 +0100

    Add randomized and animated loading icon in LogOutComponent

--- src/app/authentication/log-out/log-out.component.ts
+++ src/app/authentication/log-out/log-out.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogOutComponent implements OnInit {
 
+  readonly iconClassBase = 'fas fa-4x fa-'
+
+  iconClass: string;
+  iconClassOptions = [
+    'apple-alt', 'bacon', 'bone', 'bread-slice', 'candy-cane', 'carrot', 'cheese', 'cookie', 'drumstick-bite',
+    'egg', 'fish', 'hamburger', 'hotdog', 'ice-cream', 'lemon', 'pepper-hot', 'pizza-slice', 'stroopwafel'
+  ];
+
   constructor() { }
 
+  getRandomIconClass() {
+    this.iconClass = this.iconClassBase + this.iconClassOptions[Math.floor(Math.random() * this.iconClassOptions.length)];
+    console.log(this.iconClass);
+  }
+
+  onIconClick() {
+    this.getRandomIconClass();
+  }
+
   ngOnInit() {
+    this.getRandomIconClass();
   }
 
 }
